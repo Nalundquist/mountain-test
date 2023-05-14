d4d53dc1ef9093b602e2a603b45f7e7bf1e122d4
Noah Lundquist <nalundquist@gmail.com>
2023-05-14 15:35:55 -0700

    edit css for topbar

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -4,10 +4,12 @@ import { SelectBar } from './SelectBar';
 export const TopBar = () => {
 	return (
 		<div>
-			<div className={classes.blueBar} >pfasjhoifh</div>
+			<div className={classes.blueBar}></div>
 			<div className={classes.whiteBar}>
-				<h1 className={classes.bigNumber}>02.</h1>
-				<h3 className={classes.midText}>CLIMB</h3>
+				<div className={classes.bigBlock}>
+					<h1 className={classes.bigNumber}>02.</h1>
+					<h3 className={classes.midText}>CLIMB</h3>
+				</div>
 				<p className={classes.smallerText}>Guis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 			</div>
 			<SelectBar />
