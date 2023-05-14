489e87baf8e0ea9462a7518d16a91df4f557b62a
Noah Lundquist <nalundquist@gmail.com>
2023-05-14 16:35:30 -0700

    complete rudimentary styling on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,14 @@ import classes from "./Footer.module.css";
 export const Footer = () => {
 	return(
 		<div className={classes.footerContainer}>
-			<p>LOSANGELESMOUNTAINS</p>
+			<div className={classes.footerLogo}>
+				<div className={classes.logoCircle} />
+				<div className={classes.laMountBlock}>
+					<p>LOSANGELES</p>
+					<p>MOUNTAINS</p>
+				</div>
+			</div>
+			<p className={classes.copyright}>COPYRIGHT 2023, ALL RIGHTS RESERVED</p>
 		</div>
 	)
 }
